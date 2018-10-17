d45593e912c35109ba9796d7160aa182aaa433e3
rasmusmkj <rasmusmkjeldsen@gmail.com>
2018-10-17 14:42:14 +0200

    Skeleton + a little styling + sticky navbar JS

---
+++ external_script.js
@@ -0,0 +1,13 @@
+// Sticky Nav bar
+window.onscroll = function() {myFunction()};
+
+let navbar = document.getElementById("navbar");
+let sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
